ac5d23191521ccdeb1cdc6f27911fe4b96c2624a
Alina Demchenko <demalina88@gmail.com>
2023-06-27 10:29:37 -0700

    edited

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,14 +1,36 @@
 import axios from "axios";
+import Notiflix from 'notiflix';
+
+Notiflix.Notify.init({
+    width: '500px',
+    position: 'center-center',
+    fontSize: '20px',
+    timeout: 1000,
+    clickToClose: true,
+    failure: {
+      background:  "radial-gradient(189.32% 190.93% at 59.76% -23.42%, #4F2EE8 18.03%, #FFF 100%)",
+      notiflixIconColor: '#fff',
+    },
+  });
 
 const refs = {
     categoryList: document.querySelector(".js-category-list"),
     general: document.querySelector('li[data-id="allCategories"]'),
 }
 
-export async function getCategories(){
+async function getCategories(){
    const response = await axios.get(`https://books-backend.p.goit.global/books/category-list `);
       return response;
     }
+
+    async function getBooksByCategory (selectedCategory){
+        const response = await axios.get(`https://books-backend.p.goit.global/books/category`, {
+             params: {
+                category: selectedCategory
+             }
+           });
+           return response;
+         }
 
 getCategories().then(({data}) => {
     refs.categoryList.insertAdjacentHTML('beforeend',createCategoriesMarkup(data))})
@@ -21,15 +43,28 @@ function createCategoriesMarkup(arr){
 
 refs.categoryList.addEventListener("click", categoryPicker)
 
-function categoryPicker (evt){
+async function categoryPicker (evt){
     if(evt.target === evt.currentTarget){
         return
     }
     if (!evt.target.classList.contains("active-category")){
         const currentActiveCategory = document.querySelector(".active-category");
-        currentActiveCategory.classList.remove("active-category")
-        evt.target.classList.add("active-category")
+        currentActiveCategory.classList.remove("active-category");
+        evt.target.classList.add("active-category");
+        try {
+            const booksByCategory = await getBooksByCategory(evt.target.dataset.id);
+            console.log(booksByCategory);
+            if (!booksByCategory.data) {
+              throw new Error();
+            }
+            // books.gallery.innerHTML = renderBooks(booksByCategory.data);
+            // book_image, book_image_width, title, author, id
+          } catch (error) {
+            Notiflix.Notify.failure(
+              'Sorry, no books match this category'
+            );
+          }
     }
-}
+    }
 
 
