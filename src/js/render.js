0da2a71ef6b33a80ffb874a724faffaa95182147
Leo <fullsleons@gmail.com>
2023-06-28 20:44:18 +0300

    add function

---
+++ src/js/render.js
@@ -0,0 +1,35 @@
+export function createCategoriesMarkup(arr) {
+    return arr
+      .map(({ list_name }) => {
+        return (markup = `<li class="category-list-item" data-id="${list_name}">${list_name}</li>`);
+      })
+      .join('');
+  }
+
+ export function renderBooks (arr){
+    return arr.map(({book_image, book_image_width, title, author, _id}) => {
+    const markup = ` <li data-id="${_id}"><img
+    src="${book_image}"
+    alt="${title}"
+    width="${book_image_width}"
+    class="book-photo"
+    loading="lazy"
+    />
+    <h3 class="book-name">${title}</h3>
+    <p class="book-author">${author}</p>
+    </li>`;
+    return markup;
+    })
+ }
+    
+ export function createMarkupSupport(arr) {
+   const markup = arr
+     .map(({ title, url, img }) => {
+       return `<li class="list-item">
+        <a href="${url}">
+            <img src="${img}" alt="${title}" width="300">
+            </li>`;
+     })
+     .join('');
+   return markup;
+ }
--- src/js/support.js
+++ src/js/support.js
@@ -7,140 +7,67 @@ import razom from '../Fondu/razom@2x.png';
 import hunger from '../Fondu/hunger@2x.png';
 import emergencies from '../Fondu/emergencies@2x.png';
 import prytula from '../Fondu/prytula@2x.png';
-
-// import Swiper, { Navigation, Pagination } from 'swiper';
-// import 'swiper/swiper.min.css';
-
-// const swiper = new Swiper('.swiper', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
-
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
-
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
-
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
+import { createMarkupSupport } from './render';
 
 
+const Fonds = [
+  {
+    title: 'Save the Children',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: savechildren,
+  },
+  {
+    title: 'Project HOPE',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: hope,
+  },
+  {
+    title: 'International Medical Corps',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: intmedcorps,
+  },
+  {
+    title: 'RAZOM',
+    url: 'https://www.razomforukraine.org/',
+    img: razom,
+  },
+  {
+    title: 'Action against hunger',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: hunger,
+  },
+  {
+    title: 'Serhiy Prytula Charity Foundation',
+    url: 'https://prytulafoundation.org/en',
+    img: prytula,
+  },
+  {
+    title: 'UNITED24',
+    url: 'https://u24.gov.ua/uk',
+    img: united24,
+  },
+  {
+    title: 'Medicins Sans Frontieres',
+    url: 'https://www.msf.org/ukraine',
+    img: medicins,
+  },
+  {
+    title: 'World vision',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: emergencies,
+  },
+];
+
+const refs = {
+  list: document.querySelector('.js-list'),
+  btn: document.querySelector('.show-more'),
+  images: document.querySelectorAll('img'),
+};
+
+createMarkupSupport(Fonds);
+refs.list.insertAdjacentHTML('afterbegin', createMarkupSupport(Fonds));
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const Fonds = [
-//   {
-//     title: 'Save the Children',
-//     url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-//     img: savechildren,
-//   },
-//   {
-//     title: 'Project HOPE',
-//     url: 'https://www.projecthope.org/country/ukraine/',
-//     img: hope,
-//   },
-//   {
-//     title: 'International Medical Corps',
-//     url: 'https://internationalmedicalcorps.org/country/ukraine/',
-//     img: intmedcorps,
-//   },
-//   {
-//     title: 'RAZOM',
-//     url: 'https://www.razomforukraine.org/',
-//     img: razom,
-//   },
-//   {
-//     title: 'Action against hunger',
-//     url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-//     img: hunger,
-//   },
-//   {
-//     title: 'Serhiy Prytula Charity Foundation',
-//     url: 'https://prytulafoundation.org/en',
-//     img: prytula,
-//   },
-//   {
-//     title: 'UNITED24',
-//     url: 'https://u24.gov.ua/uk',
-//     img: united24,
-//   },
-//   {
-//     title: 'Medicins Sans Frontieres',
-//     url: 'https://www.msf.org/ukraine',
-//     img: medicins,
-//   },
-//   {
-//     title: 'World vision',
-//     url: 'https://www.wvi.org/emergencies/ukraine',
-//     img: emergencies,
-//   },
-// ];
-
-// const refs = {
-//   list: document.querySelector('.js-list'),
-//   btn: document.querySelector('.show-more'),
-//   images: document.querySelectorAll('img'),
-// };
-
-// function createMarkup(arr) {
-//   const markup = arr
-//     .map(({ title, url, img }, idx) => {
-//       return `<li class="list-item"><span class="number" >0${idx + 1}</span>
-//         <a  href="${url}">
-//             <img class="normalize-img" src="${img}" alt="${title} width="131" height="32">
-//             </a>
-//             </li>`;
-//     })
-//     .join('');
-//   return markup;
-// }
-// createMarkup(Fonds);
-// refs.list.insertAdjacentHTML('afterbegin', createMarkup(Fonds));
-
-// приховуємо всі зображення за замовчуванням
-// for (let i = 0; i < refs.images.length; i++) {
-//  refs.images[i].style.display = 'none';
-// }
-
-// додаємо обробник подій для кнопки
-// refs.btn.addEventListener('click', () => {
-//   // перевіряємо, чи зображення видимі
-//   if (refs.images[7].style.display === 'none') {
-//     // показуємо зображення
-//     for (let i = 7; i <= images.length; i++) {
-//       refs.images[i].style.display = 'block';
-//     }
-//   } else {
-//     // приховуємо зображення
-//     for (let i = 7; i <= images.length; i++) {
-//       refs.images[i].style.display = 'none';
-//     }
-//   }
-// });
 
 
 
