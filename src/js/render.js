5173f89695c9bd76005f081ccaf0c708e5947fa1
Leo <fullsleons@gmail.com>
2023-07-01 18:55:22 +0300

    add 2x

--- src/js/render.js
+++ src/js/render.js
@@ -108,10 +108,10 @@ export function renderMarkupBook(books, valueIteration, widthImg, heigthImg) {
 
 export function createMarkupSupport(arr) {
   const markup = arr
-    .map(({ title, url, img }, idx) => {
+    .map(({ title, url, img, img2 }, idx) => {
       return `<li class="list-item swiper-slide"><span class="number" >0${idx + 1}</span>
         <a  href="${url}">
-            <img class="normalize-img" src="${img}" alt="${title} height="35">
+            <img class="normalize-img" src="${img}" srcset="${img} 1x, ${img2} 2x" alt="${title} loading="laxy">
             </a>
             </li>`;
     })
