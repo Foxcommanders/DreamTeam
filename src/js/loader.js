edee37f2929434eded4ffee327ace44ee3e1abe6
DmytroKhmaruk <dima.khmaruk7@gmail.com>
2023-07-01 23:25:20 +0200

    підправив стилі

--- src/js/loader.js
+++ src/js/loader.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', function () {
-  console.log('Страница загружена');
   const loaderContainer = document.querySelector('.loader-container');
   loaderContainer.style.display = 'none';
 });
